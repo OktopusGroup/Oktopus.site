85fef8d8bc78c9191fdb6f54da19e3be2019654f
rajawat <rahul.rajawat@myqsoft.com>
2017-09-08 18:10:09 +0530

    fix social button box shadow and hover effect, box shadow on button

--- src/components/form_components/social/index.js
+++ src/components/form_components/social/index.js
@@ -3,22 +3,15 @@ import React from 'react';
 // import scss
 import css from './social.scss';
 
-// import media
-import facebook from './images/FbLogin.svg';
-import linkedin from './images/LiLogin.svg';
-
 // import form components
 import Button from '../button';
-
-const LinkedIn = <img src={linkedin} alt="LinkedIn" />;
-const Facebook = <img src={facebook} alt="Facebook" />;
 
 // Create component for button
 const Social = () => (
   <div className={css.body}>
     <ul>
-      <li><Button text={LinkedIn} classes={css.btnSocial} /></li>
-      <li><Button text={Facebook} classes={css.btnSocial} /></li>
+      <li><Button classes={[css.btnSocial, css.facebook]} /></li>
+      <li><Button classes={[css.btnSocial, css.linkedin]} /></li>
     </ul>
   </div>
 );
